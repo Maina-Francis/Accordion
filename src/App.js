05610d25ed91574438f24edf4ba1dd0b667bd440
Francis Maina <sebastianfrancix@gmail.com>
2022-12-29 23:43:29 +0300

    added app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  console.log(questions);
+
+  return (
+    <main className="container">
+      <div>
+        <h3>Questions And Answers About Login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
